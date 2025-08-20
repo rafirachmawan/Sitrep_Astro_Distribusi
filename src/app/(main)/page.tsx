f2e6d3b96bb853456081504846e1984c36435d92
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2025-08-20 16:33:55 +0700

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,10 @@
-// src/app/(app)/page.tsx
+"use client";
+
 import DashboardClient from "@/components/DashboardClient";
 import AuthGuard from "@/components/AuthGuard";
 
 export default function Page() {
   return (
-    // Jangan batasi role di sini. Cukup pastikan user sudah login.
     <AuthGuard>
       <DashboardClient />
     </AuthGuard>
